39163241a8d765885b267bba9232f7909ac45dfb
Jose Sepulveda <josealejandrosepulvedapino@gmail.com>
2024-05-01 02:12:12 -0400

    v0.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,34 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'portfolio';
 
-  selectedItem: string = '';
+  selectedItem: string = 'Sobre mi';
+  menuItems: string[] = ['Sobre mi', 'Proyectos', 'Contacto'];
+  selectedItemIndex: number = 0;
   
   selectItem(selectedItem : string) {
     this.selectedItem = selectedItem;
   }
+
+  onMouseWheel(event: WheelEvent) {
+    if (event.deltaY > 0) {
+      // Desplazamiento hacia abajo
+      this.selectNextItem();
+    } else {
+      // Desplazamiento hacia arriba
+      this.selectPreviousItem();
+    }
+    // Evita el desplazamiento predeterminado de la página
+    event.preventDefault();
+  }
+  
+  selectNextItem() {
+    this.selectedItemIndex = (this.selectedItemIndex + 1) % this.menuItems.length;
+    this.selectedItem = this.menuItems[this.selectedItemIndex];
+  }
+  
+  selectPreviousItem() {
+    this.selectedItemIndex = (this.selectedItemIndex - 1 + this.menuItems.length) % this.menuItems.length;
+    this.selectedItem = this.menuItems[this.selectedItemIndex];
+  }
+
 }
